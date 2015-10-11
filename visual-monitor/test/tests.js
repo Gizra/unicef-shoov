64889c36f586d18ec94b386e3427c53752851ce1
DavidKohav <david@gizra.com>
2015-10-11 12:17:48 +0300

    Add a visual monitor

---
+++ visual-monitor/test/tests.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var shoovWebdrivercss = require('shoov-webdrivercss');
+
+// This can be executed by passing the environment argument like this:
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=chrome mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=iphone5 mocha
+
+var capsConfig = {
+  'chrome': {
+    'browser' : 'Chrome',
+    'browser_version' : '42.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'resolution' : '1024x768'
+  },
+  'ie11': {
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1024x768'
+  },
+  'iphone5': {
+    'browser' : 'Chrome',
+    'browser_version' : '42.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'chromeOptions': {
+      'mobileEmulation': {
+        'deviceName': 'Apple iPhone 5'
+      }
+    }
+  }
+};
+
+var selectedCaps = process.env.SELECTED_CAPS || undefined;
+var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
+
+var providerPrefix = process.env.PROVIDER_PREFIX ? process.env.PROVIDER_PREFIX + '-' : '';
+var testName = selectedCaps ? providerPrefix + selectedCaps : providerPrefix + 'default';
+
+var baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://www.unicef.org';
+
+var resultsCallback = process.env.DEBUG ? console.log : shoovWebdrivercss.processResults;
+
+describe('Visual monitor testing', function() {
+
+  this.timeout(99999999);
+  var client = {};
+
+  before(function(done){
+    client = shoovWebdrivercss.before(done, caps);
+  });
+
+  after(function(done) {
+    shoovWebdrivercss.after(done);
+  });
+
+  it('should show the home page',function(done) {
+    client
+      .url(baseUrl)
+      .pause(3000)
+      .click('#ipeL122355 area[alt="close"]')
+      .webdrivercss(testName + '.homepage', {
+        name: '1',
+        exclude:
+          [
+            // Top carousel.
+            '.infiniteCarousel',
+            // Related article.
+            '.quarterFeature'
+          ],
+        remove: [],
+        hide: [],
+        screenWidth: selectedCaps == 'chrome' ? [960] : undefined,
+      }, resultsCallback)
+      .call(done);
+  });
+});
